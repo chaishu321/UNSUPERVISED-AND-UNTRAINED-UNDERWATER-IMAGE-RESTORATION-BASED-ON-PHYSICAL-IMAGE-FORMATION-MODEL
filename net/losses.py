241520dea117335487e6effff8a4da6a2a094ead
chaishu321 <71363223+chaishu321@users.noreply.github.com>
2022-10-19 11:32:20 +0800

    Update losses.py

--- net/losses.py
+++ net/losses.py
@@ -1,1 +1,89 @@
+import torch
+from torch import nn
+import numpy as np
+from torch.nn import functional
 
+
+class StdLoss(nn.Module):
+    def __init__(self):
+        """
+        Loss on the variance of the image.
+        Works in the grayscale.
+        If the image is smooth, gets zero
+        """
+        super(StdLoss, self).__init__()
+        blur = (1 / 25) * np.ones((5, 5))
+        blur = blur.reshape(1, 1, blur.shape[0], blur.shape[1])
+        self.mse = nn.MSELoss()
+        self.blur = nn.Parameter(data=torch.cuda.FloatTensor(blur), requires_grad=False)
+        image = np.zeros((5, 5))
+        image[2, 2] = 1
+        image = image.reshape(1, 1, image.shape[0], image.shape[1])
+        self.image = nn.Parameter(data=torch.cuda.FloatTensor(image), requires_grad=False)
+        self.gray_scale = GrayscaleLayer()
+
+    def forward(self, x):
+        x = self.gray_scale(x)
+        return self.mse(functional.conv2d(x, self.image), functional.conv2d(x, self.blur))
+
+
+class GrayscaleLayer(nn.Module):
+    def __init__(self):
+        super(GrayscaleLayer, self).__init__()
+
+    def forward(self, x):
+        return torch.mean(x, 1, keepdim=True)
+
+
+class TLoss(nn.Module):
+
+    def __init__(self):
+        super(TLoss, self).__init__()
+
+    def forward(self, x):
+        r, g, b = torch.split(torch.log(x), 1, dim=1)
+        w1 = r/g
+        w2 = g/b
+        w3 = b/r
+        mean_w1 = torch.mean(w1, [2, 3], keepdim=True)
+        mean_w2 = torch.mean(w2, [2, 3], keepdim=True)
+        mean_w3 = torch.mean(w3, [2, 3], keepdim=True)
+        a = w1 - mean_w1
+        b = w2 - mean_w2
+        c = w3 - mean_w3
+        k = torch.pow(a, 2) + torch.pow(b, 2) + torch.pow(c, 2)
+        return k
+
+
+class L_color(nn.Module):
+
+    def __init__(self):
+        super(L_color, self).__init__()
+
+    def forward(self, x):
+
+        b,c,h,w = x.shape
+
+        mean_rgb = torch.mean(x,[2,3],keepdim=True)
+        mr,mg, mb = torch.split(mean_rgb, 1, dim=1)
+        Drg = torch.pow(mr-mg,2)
+        Drb = torch.pow(mr-mb,2)
+        Dgb = torch.pow(mb-mg,2)
+        k = torch.pow(torch.pow(Drg,2) + torch.pow(Drb,2) + torch.pow(Dgb,2),0.5)
+        return k
+
+
+class L_TV(nn.Module):
+    def __init__(self,TVLoss_weight=1):
+        super(L_TV,self).__init__()
+        self.TVLoss_weight = TVLoss_weight
+
+    def forward(self,x):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h =  (x.size()[2]-1) * x.size()[3]
+        count_w = x.size()[2] * (x.size()[3] - 1)
+        h_tv = torch.pow((x[:,:,1:,:]-x[:,:,:h_x-1,:]),2).sum()
+        w_tv = torch.pow((x[:,:,:,1:]-x[:,:,:,:w_x-1]),2).sum()
+        return self.TVLoss_weight*2*(h_tv/count_h+w_tv/count_w)
